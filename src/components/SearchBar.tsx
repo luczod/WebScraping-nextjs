e8e58f27e0b1ffa0166c4a312d7593961ddfc860
luczod <kzod5611@gmail.com>
2024-02-10 12:04:05 -0300

    feat: add search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { FormEvent, useState } from 'react';
+
+const isValidAmazonProductURL = (url: string): boolean => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes('amazon.com') ||
+      hostname.includes('amazon.') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+
+  return false;
+};
+
+export function SearchBar() {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSumit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+    if (!isValidLink) return alert('Please provide a valid Amazon link');
+
+    try {
+      setIsLoading(true);
+
+      // Scrape the product page
+      //  const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSumit}>
+      <input
+        type="text"
+        className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+      />
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
+}
